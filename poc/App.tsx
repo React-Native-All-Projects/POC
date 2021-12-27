2d0e4f95f4dc1eae8809cf439c483ee53710dc2a
OsamaTahaluf <91789466+OsamaTahaluf@users.noreply.github.com>
2021-12-27 20:41:21 +0200

    White Label

--- poc/App.tsx
+++ poc/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text,View } from "react-native"
+import Config from "react-native-config";
 
 const App = () =>{
+  useEffect(() => {
+    console.log('Config : ' , Config);
+    
+  }, [])
   return (
     <View style={[{flex:1,alignItems:'center',justifyContent:'center'}]}>
       <Text>POC</Text>
+      <Text>ENV : {Config.APPLICATION_ID}</Text>
     </View>
   )
 }
